d5680e633713d0ae084006c0a35466e7c6d98971
adoherty8 <azdoherty91@gmail.com>
2018-06-02 14:30:25 -0400

    basic live turtle visualization implemented

--- VirtualBug.py
+++ VirtualBug.py
@@ -17,11 +17,12 @@ class VirtualBug:
         self.y = np.random.randint(self.min_y, self.max_y)
         self.heading = np.random.random() * 2 * np.pi
         self.velocity = np.random.random() * 5
-
+        self.last_moved = 0 # distance moved last
 
     def move(self):
         heading_adjust = (np.random.random() - .5) * self.noise_level
         self.heading += heading_adjust
+        old_x, old_y = self.x, self.y
         self.x += self.velocity * np.cos(self.heading)
         self.y += self.velocity * np.sin(self.heading)
         if self.x > self.max_x:
@@ -37,6 +38,8 @@ class VirtualBug:
             self.y = abs(self.y)
             self.heading = -self.heading
 
+        self.last_moved = np.sqrt((old_x - self.x)**2 + (old_y - self.y)**2)
+
 def move_trial(moves):
     positions = np.zeros((moves, 3))
     bug = VirtualBug()
@@ -48,7 +51,7 @@ def move_trial(moves):
 
 def static_plot(moves):
     fig, ax = plt.subplots()
-    ax.scatter(moves[:, 0], moves[:, 1], c=moves[:,1], alpha=0.5)
+    ax.scatter(moves[:, 0], moves[:, 1], c=moves[:, 2], alpha=0.5)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.grid(True)
@@ -56,8 +59,47 @@ def static_plot(moves):
     fig.tight_layout()
     plt.show()
 
+def dynamic_tracking(bug, moves):
+    canvas = turtle.Screen()
+    pad = 100
+    canvas.setup(bug.max_x + pad, bug.max_y + pad)
+    # coordinate system needs to be transformed by half of the bounds of the box
+    # draw box
+    t = turtle.Turtle()
+    t.penup()
+    t.setx(-bug.max_x*1./2)
+    t.sety(-bug.max_y*1./2)
+    t.pendown()
+    t.forward(bug.max_x)
+    t.left(90)
+    t.forward(bug.max_y)
+    t.left(90)
+    t.forward(bug.max_x)
+    t.left(90)
+    t.forward(bug.max_y)
+    t.penup()
+    #simulate moves
+    t.setheading(bug.heading*360./(2*np.pi))
+    t.setx(bug.x - bug.max_x * 1. / 2)
+    t.sety(bug.y -bug.max_y * 1. / 2)
+    t.pendown()
+    for i in range(moves):
+        bug.move()
+        x, y = bug.x, bug.y
+        t.setheading(bug.heading * 360. / (2 * np.pi))
+
+        t.forward(bug.last_moved)
+
+
+
+
+
+    canvas.exitonclick()
+
 
 
 if __name__ == "__main__":
-    pos = move_trial(1000)
-    static_plot(pos)
+    #pos = move_trial(1000)
+    #tatic_plot(pos)
+    b = VirtualBug()
+    dynamic_tracking(b, 1000)
