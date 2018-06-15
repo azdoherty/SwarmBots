b04d3996959645861408dd53a08bef4c90a5e53d
adoherty8 <azdoherty91@gmail.com>
2018-06-15 17:37:26 -0400

    hidden variable measurement added

--- Filters.py
+++ Filters.py
@@ -8,7 +8,8 @@ class Kalman:
     def __init__(self, x1, y1, x2, y2):
         self.xdot = x2 - x1
         self.ydot = y2 - y1
-        self.state = np.matrix([[x2, y2, self.xdot, self.ydot]])
+        self.prev_X = np.matrix([[x1, y1, self.xdot, self.ydot]])
+        self.X = np.matrix([[x2, y2, self.xdot, self.ydot]])
         self.measurement_noise = 1.0
         self.H = np.matrix([[1.0, 0.0, 0.0, 0.0],
                                     [0.0, 1.0, 0.0, 0.0],
@@ -30,7 +31,14 @@ class Kalman:
                     [0.0, 0.0, 0.0, 1.0]])
 
     def update(self, measurement):
-        # measurement
+        self.prev_X = self.X
+        xdotMeasured = measurement[0] - self.X[0]
+        ydotMeasured = measurement[1] - self.X[1]
+        Z = np.matrix([[measurement[0], measurement[1], xdotMeasured, ydotMeasured]])
+        Y = Z - self.H * self.X
+        S = self.H * self.P * self.H.transpose()
+        K = self.P * self.H.transpose()
+
 
         pass
 
